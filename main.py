ef1b3cfe52a8550691e50462f66858f00f29292f
Simo Ryu <cloneofsimo@gmail.com>
2023-11-06 03:51:42 +0000

    back to ae

--- main.py
+++ main.py
@@ -13,106 +13,58 @@ from PIL import Image
 import wandb
 
 
-def unet():
-    from diffusers import UNet2DConditionModel
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
 
-    # config referenced from https://huggingface.co/nota-ai/bk-sdm-small/blob/main/unet/config.json
-    unet = UNet2DConditionModel.from_config(
-        {
-            "act_fn": "silu",
-            "attention_head_dim": 8,
-            "block_out_channels": [32, 64, 128, 128],
-            "center_input_sample": False,
-            "class_embed_type": None,
-            "class_embeddings_concat": False,
-            "conv_in_kernel": 3,
-            "conv_out_kernel": 3,
-            "cross_attention_dim": 768,
-            "cross_attention_norm": None,
-            "down_block_types": [
-                "CrossAttnDownBlock2D",
-                "CrossAttnDownBlock2D",
-                "CrossAttnDownBlock2D",
-                "DownBlock2D",
-            ],
-            "downsample_padding": 1,
-            "dual_cross_attention": False,
-            "encoder_hid_dim": None,
-            "flip_sin_to_cos": True,
-            "freq_shift": 0,
-            "in_channels": 4,
-            "layers_per_block": 1,
-            "mid_block_only_cross_attention": None,
-            "mid_block_scale_factor": 1,
-            "mid_block_type": None,
-            "norm_eps": 1e-05,
-            "norm_num_groups": 32,
-            "num_class_embeds": None,
-            "only_cross_attention": False,
-            "out_channels": 3,
-            "projection_class_embeddings_input_dim": None,
-            "resnet_out_scale_factor": 1.0,
-            "resnet_skip_time_act": False,
-            "resnet_time_scale_shift": "default",
-            "sample_size": 64,
-            "time_cond_proj_dim": None,
-            "time_embedding_act_fn": None,
-            "time_embedding_type": "positional",
-            "timestep_post_act": None,
-            "up_block_types": [
-                "UpBlock2D",
-                "CrossAttnUpBlock2D",
-                "CrossAttnUpBlock2D",
-                "CrossAttnUpBlock2D",
-            ],
-            "upcast_attention": False,
-            "use_linear_projection": False,
-        }
-    )
-
-    return unet
-
-
-blk = lambda ic, oc: nn.Sequential(
-    nn.Conv2d(ic, oc, 7, padding=3),
-    nn.BatchNorm2d(oc),
-    nn.LeakyReLU(),
-)
-
-
-class DummyEpsModel(nn.Module):
-    """
-    This should be unet-like, but let's don't think about the model too much :P
-    Basically, any universal R^n -> R^n model should work.
-    """
-
-    def __init__(self, n_channel: int) -> None:
-        super(DummyEpsModel, self).__init__()
-        self.conv = nn.Sequential(  # with batchnorm
-            blk(n_channel, 64),
-            blk(64, 128),
-            blk(128, 256),
-            blk(256, 512),
-            blk(512, 256),
-            blk(256, 128),
-            blk(128, 64),
-            nn.Conv2d(64, n_channel, 3, padding=1),
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(512, 512, kernel_size=4, stride=1, padding=0),
         )
 
-    def forward(self, x) -> torch.Tensor:
-        # Lets think about using t later. In the paper, they used Tr-like positional embeddings.
-        return self.conv(x)
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(512, 512, kernel_size=4, stride=1, padding=0),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1),
+            nn.Tanh(),
+        )
 
+        # initialize all weights with Isotropic gaussian (µ = 0, σ = 0.02), bias with Constant(0)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight, 0, 0.02)
+                nn.init.constant_(m.bias, 0)
 
-def dummy_unet():
-    # https://github.com/cloneofsimo/minDiffusion/blob/master/superminddpm.py
-    return DummyEpsModel(3)
+    def forward(self, x):
+        z = self.encoder(x)
+        x = self.decoder(z)
+        return x
 
 
 class RandomMatrix:
     def __init__(self, dataset):
         # calculate FFT of the datapoints
-        n = 10000
+        n = 1000
         datapoints = []
 
         for x, y in dataset:
@@ -145,9 +97,9 @@ class RandomMatrix:
 def train(
     batch_size=256,
     lr=1e-4,
-    validate_step=50,
+    validate_step=100,
     epochs=1000,
-    tight_lambda=1,
+    tight_lambda=0.1,
     copy_interval=1,
 ):
     wandb.init(
@@ -165,7 +117,11 @@ def train(
 
     # prepare data
     tf = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            transforms.Resize((64, 64)),
+        ]
     )
     dataset = CIFAR10(
         "./data",
@@ -180,7 +136,7 @@ def train(
 
     device = "cuda:0"
 
-    model = dummy_unet()
+    model = Autoencoder()
     model.to(device)
     model.train()
     optimizer = Adam(
@@ -188,7 +144,7 @@ def train(
         lr=lr,
     )
 
-    model_copy = dummy_unet()
+    model_copy = Autoencoder()
     model_copy.to(device)
 
     model_copy.requires_grad_(False)
@@ -208,37 +164,51 @@ def train(
             if global_step % validate_step == 0:
                 # validate model by sampling
                 with torch.no_grad():
-                    z = rmg(None, batch_size=32)
+                    z = rmg(None, batch_size=25)
                     z = z.to(device)
                     model.eval()
                     fz = model(z)
                     print(fz.shape)
                     model.train()
-                    grid = make_grid(fz, nrow=4, normalize=True, value_range=(-1, 1))
-                    save_image(grid, f"./contents/ign_{global_step}.png")
+                    fz_grid = make_grid(fz, nrow=5, normalize=True, value_range=(-1, 1))
+                    save_image(fz_grid, f"./contents/ign_{global_step}.png")
 
-                    grid = grid.cpu()
-                    grid = (grid * 255).byte()
-                    grid = grid.permute(1, 2, 0).numpy()
-                    grid = Image.fromarray(grid)
-                    wandb.log({"sample": [wandb.Image(grid)]})
+                    fz_grid = fz_grid.cpu()
+                    fz_grid = (fz_grid * 255).byte()
+                    fz_grid = fz_grid.permute(1, 2, 0).numpy()
+                    fz_grid = Image.fromarray(fz_grid)
+
+                    z_grid = make_grid(z, nrow=5, normalize=True, value_range=(-1, 1))
+                    z_grid = z_grid.cpu()
+                    z_grid = (z_grid * 255).byte()
+                    z_grid = z_grid.permute(1, 2, 0).numpy()
+                    z_grid = Image.fromarray(z_grid)
+
+                    wandb.log({"sample": [wandb.Image(fz_grid)]})
+                    wandb.log({"z": [wandb.Image(z_grid)]})
 
             x = x.to(device)
             z = rmg(None, batch_size=batch_size)
             z = z.to(device)
 
             fx = model(x)
-            loss_rec = (fx - x).pow(2).mean()
+            loss_rec = (fx - x).abs().mean()
             loss_rec.backward()
 
             fz = model(z)
             f_fz = model_copy(fz)
-            loss_idem = (f_fz - fz).pow(2).mean()
+            loss_idem = (f_fz - fz).abs().mean()
             loss_idem.backward()
 
             f_z = fz.detach()
             ff_z = model(f_z)
-            loss_tight = -(ff_z - f_z).pow(2).mean() * tight_lambda
+            loss_tight = -(ff_z - f_z).abs().reshape(batch_size, -1).mean(1)
+            with torch.no_grad():
+                l_rec_z = (f_z - z).abs().reshape(batch_size, -1).mean(1) * 1.5
+
+            loss_tight = torch.tanh(loss_tight / l_rec_z) * l_rec_z * tight_lambda
+            loss_tight = loss_tight.mean()
+
             loss_tight.backward()
 
             pbar.set_description(
@@ -252,7 +222,7 @@ def train(
                     "loss_tight": loss_tight.item(),
                     "overall_loss": loss_rec.item()
                     + loss_idem.item()
-                    + loss_tight.item() * tight_lambda,
+                    + loss_tight.item(),
                 }
             )
 
